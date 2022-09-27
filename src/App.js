d64588b735b54f47b42a208acd06870292f56e79
ShiVA-ru <secman@bk.ru>
2022-09-27 22:26:34 +0500

    Lesson 8 adaptive home and cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Categories from './components/Categories';
+import React from 'react';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
-import Sort from './components/Sort';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { Routes, Route } from "react-router-dom";
@@ -16,15 +12,11 @@ const App = () => {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
           <Routes >
             <Route path='/' element={<Home />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
-          {/* <Home /> */}
-          {/* <NotFound /> */}
-        </div>
       </div>
     </div>
   )
