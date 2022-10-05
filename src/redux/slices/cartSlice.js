487e407e1a892156576cecf2486f1e9bd62b202b
ShiVA-ru <68607784+ShiVA-ru@users.noreply.github.com>
2022-10-05 18:29:52 +0500

    lesson 16 transfer commit

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,12 +10,6 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    // addItem(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => {
-    //     return obj.price + sum
-    //   }, 0);
-    // },
     addItem(state, action) {
       const findItem = state.items.find(obj => obj.id === action.payload.id);
 
@@ -27,19 +21,32 @@ const cartSlice = createSlice({
           count: 1,
         });
       }
+
       state.totalPrice = state.items.reduce((sum, obj) => {
         return obj.price * obj.count + sum
       }, 0);
     },
+    minusItem(state, action) {
+      const findItem = state.items.find(obj => obj.id === action.payload);
+
+      if (findItem.count > 1) {
+        findItem.count--;
+      } else {
+        if (window.confirm('Вы действительно хотите удалить пиццу?')) {
+          state.items = state.items.filter(obj => obj.id !== action.payload);
+        }
+      }
+    },
     removeItem(state, action) {
-      state.items.filter(obj => obj.id !== action.payload);
+      state.items = state.items.filter(obj => obj.id !== action.payload);
     },
     clearItems(state) {
       state.items = [];
+      state.totalPrice = 0;
     }
   }
 });
 
-export const { addItem, removeItem, clearItems } = cartSlice.actions;
+export const { addItem, minusItem, removeItem, clearItems } = cartSlice.actions;
 
 export default cartSlice.reducer;
